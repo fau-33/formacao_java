a1159859299335c3905b09ecbde7c02bda18f493
Flávio Félix <fau_felix30@hotmail.com>
2025-10-13 20:34:10 -0300

    Start convertion

---
+++ src/fundamentos/CoversaoDeNumeroParaString.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class CoversaoDeNumeroParaString {
+    public static void main(String[] args) {
+        int n1 = 123456;
+        double n2 = 7891011;
+
+        String s1 = n1 + "";
+        String s2 = n2 + "";
+
+        System.out.println(s1 + " " + s2);
+
+        Integer n3 = 1234;
+        System.out.println(n3.toString());
+
+        System.out.println(String.format("%d", n1));
+    }
+}
